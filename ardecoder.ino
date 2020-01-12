77a92fb6797e6c9d16b224b94aff8c4e99393619
Nicola Fontana <ntd@entidi.it>
2020-01-12 15:50:45 +0100

    Test and refactor whole code

--- ardecoder.ino
+++ ardecoder.ino
@@ -1,6 +1,8 @@
 /* Ardecoder: decoding up to 4 encoder with Arduino Uno or Nano.
  *
- * Expected connections:
+ *
+ * ELECTRICAL CONNECTIONS
+ * ----------------------
  *
  * D0   Encoder 1 (A)
  * D1   Encoder 1 (B)
@@ -16,75 +18,20 @@
  * D10  Encoder 3 (Z)
  * D11  Encoder 4 (Z)
  *
- * If fewer encoders are needed, just void connecting them.
- */
-#include <stdint.h>
-
-#define DEBUGGING 1
-#define OVERFLOW  1
-
-
-volatile int16_t raw[4] = { 0, 0, 0, 0 };
-
-#if OVERFLOW
-volatile uint8_t overflow[4] = { 0, 0, 0, 0 };
-volatile int8_t  last[4] = { 0, 0, 0, 0 };
-#endif
-
-char line[32] = { 0 };
-char *ptr     = line;
-
-
-void setup()
-{
-    int pin;
-
-    /* Set D0..D11 in input mode */
-    for (pin = 0; pin <= 11; ++pin) {
-        pinMode(pin, INPUT);
-    }
-
-    // Enable interrupt on any change of D0..D7
-    PCMSK2 = 0xFF;
-    PCIFR |= bit(PCIF2);
-    PCICR |= bit(PCIE2);
-
-    // Enable interrupt on any change of D8..D11
-    PCMSK0 = 0x0F;
-    PCIFR |= bit(PCIF0);
-    PCICR |= bit(PCIE0);
-
-    Serial.begin(115200);
-    Serial.print("Started ardecoder\n");
-}
-
-void loop()
-{
-    char c = Serial.read();
-    if (c == '\n' || c == '\r') {
-        *ptr = '\0';
-        if (DEBUGGING) {
-            Serial.print("Received command: '");
-            Serial.print(line);
-            Serial.print("'\n");
-        }
-        ptr = line;
-    } else if (c != -1) {
-        *ptr = (char) c;
-        ++ptr;
-    }
-}
-
-/* How decoding works.
+ * If fewer encoders are needed, just avoid connecting them.
  *
- * The code keeps the last state of A B phase (old) and compares it to
- * the new state (now). Any variation will increase or decrease the
- * counter (raw) depending on a lookup table (lut).
  *
- * Furthermore, it is checked if a step is skipped (e.g. because the
- * encoder is running too quick) by leveraging another lookup table
- * (skp). If that is the case, the counter is incremented or decremented
- * by 2, depending on the last adjustment.
+ * HOW DECODING WORKS
+ * ------------------
+ *
+ * The code keeps the last state of A B phase (`old`) and compares it to
+ * the new state (`now`). Any variation will increase or decrease the
+ * counter (`raw`) depending on a lookup table (`lut`).
+ *
+ * If `OVERFLOW` is defined, it is checked if a step is skipped (e.g.
+ * when the encoder is turning too quickly) by leveraging another lookup
+ * table (`skp`). If that is the case, the counter is incremented or
+ * decremented by 2, depending on the last adjustment.
  *
  *                  _______         _______
  *         A ______|       |_______|       |______
@@ -110,14 +57,40 @@ void loop()
  *  1 1 0 1  -1 false
  *  1 1 1 0  +1 false
  *  1 1 1 1   0 false  No movement
+ *
+ * All 4 counters are adjusted according to the above decoding algorithm
+ * by the PCINT2 interrupt handler, called whenever any of the PIND bits
+ * (i.e. any D0..D7 digital input) changes.
  */
-const int8_t lut[] = {
+#include <stdint.h>
+
+#define DEBUGGING 1
+#define OVERFLOW  1
+
+
+#if DEBUGGING
+volatile bool error = false;
+
+ISR(BADISR_vect)
+{
+    error = true;
+}
+#endif
+
+volatile int16_t raw[4]   = { 0 };
+bool             homed[4] = { false };
+char             line[32] = { 0 };
+char *           ptr      = line;
+const int8_t     lut[]    = {
      0, +1, -1,  0,
     -1,  0,  0, +1,
     +1,  0,  0, -1,
      0, -1, +1,  0
 };
+
 #if OVERFLOW
+volatile uint8_t skips[4] = { 0 };
+volatile int8_t  last[4]  = { 0 };
 const bool skp[] = {
      false,  false,  false,  true,
      false,  false,  true,   false,
@@ -125,6 +98,7 @@ const bool skp[] = {
      true,   false,  false,  false
 };
 #endif
+
 
 static void
 update_counter(int n, uint8_t baba)
@@ -134,17 +108,13 @@ update_counter(int n, uint8_t baba)
 #if OVERFLOW
     if (skp[baba]) {
         raw[n] += last[n] * 2;
-        ++overflow[n];
+        ++skips[n];
     } else {
         last[n] = delta;
     }
 #endif
 }
 
-/**
- * Adjust the counters depending on the encoder phase changes.
- * PIND holds the states of digital inputs D0..D7.
- */
 ISR(PCINT2_vect)
 {
     uint8_t now = PIND;
@@ -158,6 +128,14 @@ ISR(PCINT2_vect)
     old = now;
 }
 
+static void
+reset_counter(int n, uint8_t mask)
+{
+    bool home = mask > 0;
+    homed[n] = homed[n] || home;
+    raw[n]  *= home ? 0 : 1;
+}
+
 /**
  * Reset counters on zero signal.
  * PINB holds the states of digital inputs D8..D13.
@@ -166,8 +144,87 @@ ISR(PCINT0_vect)
 {
     uint8_t bits = PINB;
 
-    raw[0] *= ! (bits & 0x01);
-    raw[1] *= ! (bits & 0x02);
-    raw[2] *= ! (bits & 0x04);
-    raw[3] *= ! (bits & 0x08);
+    reset_counter(0, bits & 0x01);
+    reset_counter(1, bits & 0x02);
+    reset_counter(2, bits & 0x04);
+    reset_counter(3, bits & 0x08);
+}
+
+static void
+setup()
+{
+    /* Set D0..D11 in input mode */
+    for (int pin = 0; pin <= 11; ++pin) {
+        pinMode(pin, INPUT_PULLUP);
+    }
+
+    // Enable interrupt on any change of D0..D7
+    PCMSK2 = 0xFF;
+    PCIFR |= bit(PCIF2);
+    PCICR |= bit(PCIE2);
+
+    // Enable interrupt on any change of D8..D11
+    PCMSK0 = 0x0F;
+    PCIFR |= bit(PCIF0);
+    PCICR |= bit(PCIE0);
+
+    Serial.begin(115200);
+    Serial.println("!Started ardecoder");
+}
+
+static void
+response(int nenc)
+{
+    Serial.print(nenc);
+    Serial.print(" ");
+    Serial.print(raw[nenc - 1]);
+    Serial.print(" ");
+    Serial.print(homed[nenc] ? "1" : "0");
+#if OVERFLOW
+    Serial.print(" ");
+    Serial.print(skips[nenc]);
+#endif
+#if DEBUGGING
+    Serial.print(" ");
+    Serial.print(error ? 1 : 0);
+#endif
+    Serial.print("\n");
+}
+
+static bool
+parse_command(const char *command)
+{
+    if (command[1] == '\0') {
+        int nenc = command[0] - '0';
+        if (nenc >= 1 && nenc <=4) {
+            response(nenc);
+            return true;
+        }
+    }
+    return false;
+}
+
+static void
+loop()
+{
+    char c = Serial.read();
+    if (c == '\n' || c == '\r') {
+        *ptr = '\0';
+#if DEBUGGING
+        /* Echo of the command */
+        Serial.print("#'");
+        Serial.print(line);
+        Serial.print("'\n");
+#endif
+        if (! parse_command(line)) {
+            /* Unrecognized command */
+            Serial.print("?'");
+            Serial.print(line);
+            Serial.print("'\n");
+        }
+        ptr = line;
+    } else if (c != -1 && ptr - line < 32) {
+        *ptr = (char) c;
+        ++ptr;
+    }
 }
