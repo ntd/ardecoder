49b4073a93ea19d2847a1eeef2d7cb02d1112ce8
Nicola Fontana <ntd@entidi.it>
2020-01-13 09:41:47 +0100

    Refactor code
    Group encoder-related data into the `Encoder` struct to better organize
    the code. Prefix functions operating on `Encoder` with `encoder_`.
    Further renaming for general tidy up.

--- ardecoder.ino
+++ ardecoder.ino
@@ -7,63 +7,89 @@
 #define OVERFLOW  1
 
 
-volatile int16_t raw[4]   = { 0 };
-bool             homed[4] = { false };
-char             line[32] = { 0 };
-char *           ptr      = line;
-const int8_t     lut[]    = {
-     0, +1, -1,  0,
-    -1,  0,  0, +1,
-    +1,  0,  0, -1,
-     0, -1, +1,  0
-};
-
+typedef struct {
+    volatile int16_t raw;
+    bool             homed;
 #if OVERFLOW
-volatile uint8_t skips[4] = { 0 };
-volatile int8_t  last[4]  = { 0 };
-const bool skp[] = {
-     false,  false,  false,  true,
-     false,  false,  true,   false,
-     false,  true,   false,  false,
-     true,   false,  false,  false
-};
+    volatile uint8_t skips;
+    volatile int8_t  last;
 #endif
+} Encoder;
+
+static Encoder encoders[4] = { 0 };
 
 
 static void
-update_counter(int n, uint8_t baba)
+encoder_update(Encoder *encoder, uint8_t baba)
 {
-    int8_t delta = lut[baba];
-    raw[n] += delta;
+    int8_t delta;
+    static const int8_t lut[] = {
+         0, +1, -1,  0,
+        -1,  0,  0, +1,
+        +1,  0,  0, -1,
+         0, -1, +1,  0
+    };
+
 #if OVERFLOW
+    static const bool skp[] = {
+         false,  false,  false,  true,
+         false,  false,  true,   false,
+         false,  true,   false,  false,
+         true,   false,  false,  false
+    };
     if (skp[baba]) {
-        raw[n] += last[n] * 2;
-        ++skips[n];
+        delta = encoder->last * 2;
+        ++encoder->skips;
     } else {
-        last[n] = delta;
+        encoder->last = delta = lut[baba];
     }
+#else
+    delta = lut[baba];
 #endif
+
+    encoder->raw += delta;
 }
 
+static void
+encoder_reset(Encoder *encoder, uint8_t mask)
+{
+    if (mask > 0) {
+        encoder->homed = true;
+        encoder->raw   = 0;
+    }
+}
+
+static void
+encoder_dump_status(const Encoder *encoder)
+{
+    Serial.print(encoder - encoders);
+    Serial.print(" ");
+    Serial.print(encoder->raw);
+    Serial.print(" ");
+    Serial.print(encoder->homed ? "1" : "0");
+#if OVERFLOW
+    Serial.print(" ");
+    Serial.print(encoder->skips);
+#endif
+    Serial.print("\n");
+}
+
+
+/**
+ * Update raw counters on AB channel changes.
+ * PIND holds the states of digital inputs D0..D7.
+ */
 ISR(PCINT2_vect)
 {
     uint8_t now = PIND;
     static uint8_t old = 0x00;
 
-    update_counter(0, ((now & 0x03) << 2) + (old & 0x03));
-    update_counter(1,  (now & 0x0C) + ((old & 0x0C) >> 2));
-    update_counter(2, ((now & 0x30) + ((old & 0x30) >> 2)) >> 2);
-    update_counter(3, ((now & 0xC0) + ((old & 0xC0) >> 2)) >> 4);
+    encoder_update(encoders + 0, ((now & 0x03) << 2) + (old & 0x03));
+    encoder_update(encoders + 1,  (now & 0x0C) + ((old & 0x0C) >> 2));
+    encoder_update(encoders + 2, ((now & 0x30) + ((old & 0x30) >> 2)) >> 2);
+    encoder_update(encoders + 3, ((now & 0xC0) + ((old & 0xC0) >> 2)) >> 4);
 
     old = now;
-}
-
-static void
-reset_counter(int n, uint8_t mask)
-{
-    bool home = mask > 0;
-    homed[n] = homed[n] || home;
-    raw[n]  *= home ? 0 : 1;
 }
 
 /**
@@ -74,10 +100,10 @@ ISR(PCINT0_vect)
 {
     uint8_t bits = PINB;
 
-    reset_counter(0, bits & 0x01);
-    reset_counter(1, bits & 0x02);
-    reset_counter(2, bits & 0x04);
-    reset_counter(3, bits & 0x08);
+    encoder_reset(encoders + 0, bits & 0x01);
+    encoder_reset(encoders + 1, bits & 0x02);
+    encoder_reset(encoders + 2, bits & 0x04);
+    encoder_reset(encoders + 3, bits & 0x08);
 }
 
 static void
@@ -88,42 +114,28 @@ setup()
         pinMode(pin, INPUT_PULLUP);
     }
 
-    // Enable interrupt on any change of D0..D7
+    /* Enable interrupt on any change of D0..D7 */
     PCMSK2 = 0xFF;
     PCIFR |= bit(PCIF2);
     PCICR |= bit(PCIE2);
 
-    // Enable interrupt on any change of D8..D11
+    /* Enable interrupt on any change of D8..D11 */
     PCMSK0 = 0x0F;
     PCIFR |= bit(PCIF0);
     PCICR |= bit(PCIE0);
 
+    /* Setup serial communication on USB */
     Serial.begin(115200);
     Serial.println("!Started ardecoder");
 }
 
-static void
-response(int nenc)
-{
-    Serial.print(nenc);
-    Serial.print(" ");
-    Serial.print(raw[nenc - 1]);
-    Serial.print(" ");
-    Serial.print(homed[nenc] ? "1" : "0");
-#if OVERFLOW
-    Serial.print(" ");
-    Serial.print(skips[nenc]);
-#endif
-    Serial.print("\n");
-}
-
 static bool
-parse_command(const char *command)
+handle_request(const char *request)
 {
-    if (command[1] == '\0') {
-        int nenc = command[0] - '0';
-        if (nenc >= 1 && nenc <=4) {
-            response(nenc);
+    if (request[1] == '\0') {
+        int n = request[0] - '0';
+        if (n >= 1 && n <=4) {
+            encoder_dump_status(encoders + n - 1);
             return true;
         }
     }
@@ -133,23 +145,25 @@ parse_command(const char *command)
 static void
 loop()
 {
+    static char request[32] = { 0 };
+    static char *ptr = request;
     char c = Serial.read();
     if (c == '\n' || c == '\r') {
         *ptr = '\0';
 #if ECHO
-        /* Echo of the command */
+        /* Echo of the request */
         Serial.print("#'");
-        Serial.print(line);
+        Serial.print(request);
         Serial.print("'\n");
 #endif
-        if (! parse_command(line)) {
-            /* Unrecognized command */
+        if (! handle_request(request)) {
+            /* Unrecognized request */
             Serial.print("?'");
-            Serial.print(line);
+            Serial.print(request);
             Serial.print("'\n");
         }
-        ptr = line;
-    } else if (c != -1 && ptr - line < 32) {
+        ptr = request;
+    } else if (c != -1 && ptr - request < 32) {
         *ptr = (char) c;
         ++ptr;
     }
